4a29da8f5f4970ed46a5ba17ac86884f288de877
Guillaume Brioudes <guillaume.brioudes@myllaume.fr>
2020-02-23 16:35:52 +0100

    Beautify et minifier

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,64 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
-sass.compiler = require('node-sass');
-var cleanCSS = require('gulp-clean-css');
-var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('default', function () {
     console.log('Hello world');
-})
+});
 
 // https://www.npmjs.com/package/gulp-sass
-// https://www.npmjs.com/package/gulp-clean-css
+var sass = require('gulp-sass');
+sass.compiler = require('node-sass');
 // https://www.npmjs.com/package/gulp-autoprefixer
+var autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('sass', function () {
+gulp.task('css', function () {
     return gulp.src('./gen/sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(cleanCSS())
         .pipe(autoprefixer({
             cascade: false
         }))
         .pipe(gulp.dest('./assets'));
 });
 
+// https://www.npmjs.com/package/gulp-concat
+var concat = require('gulp-concat');
+
+gulp.task('js', function () {
+    return gulp.src('./gen/scripts/*.js')
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('./assets/'));
+});
+
+https://www.npmjs.com/package/gulp-clean-css
+var minifyCss = require('gulp-clean-css');
+
+gulp.task('minifyCSS', function () {
+    return gulp.src('./assets/*.css')
+    .pipe(minifyCss())
+    .pipe(gulp.dest('./assets/'));
+});
+
+// https://www.npmjs.com/package/gulp-minify
+var minifyJs = require('gulp-minify');
+
+gulp.task('minifyJS', function () {
+    return gulp.src('./assets/*.js')
+    .pipe(minifyJs({
+        ext:{
+            src:'main.js'
+        }
+    }))
+    .pipe(gulp.dest('./assets/'));
+});
+
+gulp.task('minify', gulp.series('minifyCSS',  'minifyJS'));
+
 gulp.task('watch', function () {
-    gulp.watch('./gen/sass/**/*.scss', gulp.series('sass'))
+    gulp.watch('./gen/sass/**/*.scss', gulp.series('css'))
         .on('change', function (event) {
-            console.log('CSS upted');
+            console.log('CSS updated');
+        });
+    gulp.watch('./gen/scripts/*.js', gulp.series('js'))
+        .on('change', function (event) {
+            console.log('JS updated');
         });
 });
