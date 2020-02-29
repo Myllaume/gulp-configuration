2478793e0ae6b0f3018276fbed8581b7e4a1bf44
Guillaume Brioudes <guillaume.brioudes@myllaume.fr>
2020-02-29 11:01:57 +0100

    Commentaires et essaie sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,26 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync');
+
+/**
+ * -----------------------
+ * Commande par DÉFAUT
+ * ---
+ * - Affichage du message "Hello world"
+ * -----------------------
+ */
 
 gulp.task('default', function () {
     console.log('Hello world');
 });
+
+/**
+ * -----------------------
+ * Commandes de GÉNÉRATION
+ * ---
+ * - $ gulp css => compliation SCSS -> main.css + prefixage
+ * - $ gulp js => concatenation JS -> main.js
+ * -----------------------
+ */
 
 // https://www.npmjs.com/package/gulp-sass
 var sass = require('gulp-sass');
@@ -11,30 +29,44 @@ sass.compiler = require('node-sass');
 var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('css', function () {
-    return gulp.src('./gen/sass/**/*.scss')
+    return gulp.src('./dist/sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer({
             cascade: false
         }))
-        .pipe(gulp.dest('./assets'));
+        .pipe(gulp.dest('./assets'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
 });
 
 // https://www.npmjs.com/package/gulp-concat
 var concat = require('gulp-concat');
 
 gulp.task('js', function () {
-    return gulp.src('./gen/scripts/*.js')
-    .pipe(concat('main.js'))
-    .pipe(gulp.dest('./assets/'));
+    return gulp.src('./dist/scripts/*.js')
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('./assets/'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
 });
 
-https://www.npmjs.com/package/gulp-clean-css
-var minifyCss = require('gulp-clean-css');
+/**
+ * -----------------------
+ * Commandes de MINIFICATION
+ * ---
+ * - $ gulp minify => minification JS et CSS
+ * -----------------------
+ */
+
+https: //www.npmjs.com/package/gulp-clean-css
+    var minifyCss = require('gulp-clean-css');
 
 gulp.task('minifyCSS', function () {
     return gulp.src('./assets/*.css')
-    .pipe(minifyCss())
-    .pipe(gulp.dest('./assets/'));
+        .pipe(minifyCss())
+        .pipe(gulp.dest('./assets/'));
 });
 
 // https://www.npmjs.com/package/gulp-minify
@@ -42,22 +74,41 @@ var minifyJs = require('gulp-minify');
 
 gulp.task('minifyJS', function () {
     return gulp.src('./assets/*.js')
-    .pipe(minifyJs({
-        ext:{
-            src:'main.js'
-        }
-    }))
-    .pipe(gulp.dest('./assets/'));
+        .pipe(minifyJs({
+            ext: {
+                src: 'main.js'
+            }
+        }))
+        .pipe(gulp.dest('./assets/'));
 });
 
-gulp.task('minify', gulp.series('minifyCSS',  'minifyJS'));
+gulp.task('minify', gulp.series('minifyCSS', 'minifyJS'));
+
+/**
+ * -----------------------
+ * Commande d'AUTOMATISATION
+ * ---
+ * - $ gulp sync => actualisation navigateurs
+ * - $ gulp watch => activation des commandes de GÉNÉRATION
+ * -----------------------
+ */
+
+gulp.task('sync', gulp.series('php'), function () {
+    browserSync({
+        server: {
+            baseDir: './',
+            proxy: 'localhost',
+            port: 8888
+        },
+    })
+});
 
 gulp.task('watch', function () {
-    gulp.watch('./gen/sass/**/*.scss', gulp.series('css'))
+    gulp.watch('./dist/sass/**/*.scss', gulp.series('css'))
         .on('change', function (event) {
             console.log('CSS updated');
         });
-    gulp.watch('./gen/scripts/*.js', gulp.series('js'))
+    gulp.watch('./dist/scripts/*.js', gulp.series('js'))
         .on('change', function (event) {
             console.log('JS updated');
         });
